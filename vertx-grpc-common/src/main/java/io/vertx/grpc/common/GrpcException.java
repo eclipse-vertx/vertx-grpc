5dac55e309ea64c65696cc4b0868d244c6a2b182
Benjamin Berman <ben@hiddenswitch.com>
2023-06-29 20:59:50 -0700

    fix exception format

--- vertx-grpc-common/src/main/java/io/vertx/grpc/common/GrpcException.java
+++ vertx-grpc-common/src/main/java/io/vertx/grpc/common/GrpcException.java
@@ -1,37 +1,39 @@
 package io.vertx.grpc.common;
 
+import io.grpc.Status;
+import io.grpc.StatusException;
+import io.grpc.StatusRuntimeException;
 import io.vertx.core.http.HttpClientResponse;
 
-public class GrpcException extends RuntimeException {
+public class GrpcException extends StatusRuntimeException {
 
   private static final long serialVersionUID = -7838327176604697641L;
-
-  private GrpcStatus status;
 
   private HttpClientResponse httpResponse;
 
   public GrpcException(String msg, GrpcStatus status, HttpClientResponse httpResponse) {
-    super(msg);
-    this.status = status;
+    super(Status.fromCodeValue(status.code).withDescription(msg));
     this.httpResponse = httpResponse;
   }
 
+  public GrpcException(StatusRuntimeException statusRuntimeException) {
+    super(Status.fromThrowable(statusRuntimeException));
+  }
+
   public GrpcException(GrpcStatus status) {
-    this.status = status;
+    super(Status.fromCodeValue(status.code));
   }
 
   public GrpcException(GrpcStatus status, Throwable err) {
-    super(err);
-    this.status = status;
+    super(err instanceof StatusRuntimeException ? Status.fromThrowable(err) : err instanceof StatusException ? Status.fromThrowable(err) : Status.fromCodeValue(status.code).withCause(err));
   }
 
   public GrpcException(GrpcStatus status, String msg) {
-    super(msg);
-    this.status = status;
+    super(Status.fromCodeValue(status.code).withDescription(msg));
   }
 
   public GrpcStatus status() {
-    return status;
+    return GrpcStatus.valueOf(getStatus().getCode().value());
   }
 
   public HttpClientResponse response() {
