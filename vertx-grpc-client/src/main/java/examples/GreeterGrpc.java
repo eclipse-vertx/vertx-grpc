1db452f95d9014b050a3544f576bf6a82e446c5c
Lan Liang <gcslyp@gmail.com>
2023-10-11 20:58:40 +0800

    jakarta

--- vertx-grpc-client/src/main/java/examples/GreeterGrpc.java
+++ vertx-grpc-client/src/main/java/examples/GreeterGrpc.java
@@ -7,7 +7,7 @@ import static io.grpc.MethodDescriptor.generateFullMethodName;
  * The greeting service definition.
  * </pre>
  */
-@javax.annotation.Generated(
+@jakarta.annotation.Generated(
     value = "by gRPC proto compiler (version 1.44.0)",
     comments = "Source: helloworld.proto")
 @io.grpc.stub.annotations.GrpcGenerated
