e39bac375dd4fbb6860d520896e1bcec7b629d7f
Julien Viet <julien@julienviet.com>
2024-07-21 15:18:04 +0200

    Add missing module-info entries for testing in IDE

--- vertx-grpc-client/src/test/java/module-info.java
+++ vertx-grpc-client/src/test/java/module-info.java
@@ -2,11 +2,13 @@ open module io.vertx.tests.client {
   requires io.grpc;
   requires io.grpc.stub;
   requires io.grpc.util;
+  requires io.grpc.protobuf;
   requires io.vertx.core;
   requires io.vertx.grpc.client;
   requires io.vertx.grpc.common;
   requires io.vertx.testing.unit;
   requires io.vertx.tests.common;
   requires junit;
-    requires com.google.protobuf;
+  requires com.google.protobuf;
+  requires com.google.common;
 }
--- vertx-grpc-server/src/test/java/module-info.java
+++ vertx-grpc-server/src/test/java/module-info.java
@@ -1,8 +1,10 @@
 open module io.vertx.tests.server {
   requires com.google.protobuf;
+  requires com.google.common;
   requires io.grpc;
   requires io.grpc.stub;
   requires io.grpc.util;
+  requires io.grpc.protobuf;
   requires io.vertx.core;
   requires io.vertx.grpc.common;
   requires io.vertx.grpc.server;
