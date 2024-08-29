7d970512107bfd1d2d3a63188af05d256a6a8554
Julien Viet <julien@julienviet.com>
2024-08-29 10:30:31 +0200

    Upgrade io.grpc to 1.65.0

--- vertx-grpc-client/src/main/java/examples/StreamingGrpc.java
+++ vertx-grpc-client/src/main/java/examples/StreamingGrpc.java
@@ -8,7 +8,7 @@ import static io.grpc.MethodDescriptor.generateFullMethodName;
  * </pre>
  */
 @javax.annotation.Generated(
-    value = "by gRPC proto compiler (version 1.61.0)",
+    value = "by gRPC proto compiler (version 1.65.0)",
     comments = "Source: streaming.proto")
 @io.grpc.stub.annotations.GrpcGenerated
 public final class StreamingGrpc {
