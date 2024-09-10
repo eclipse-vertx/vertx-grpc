b1dbfb1cd9e9cfe0d177debb3812fa8b9e65d849
Julien Viet <julien@julienviet.com>
2024-09-10 09:08:33 +0200

    Bump vertx5-parent to 4 and adjust compiler processor configuration

--- vertx-grpc-server/src/main/java/module-info.java
+++ vertx-grpc-server/src/main/java/module-info.java
@@ -3,7 +3,7 @@ module io.vertx.grpc.server {
   requires io.netty.codec;
   requires io.vertx.core.logging;
   requires transitive io.vertx.grpc.common;
-  requires static vertx.docgen;
+  requires static io.vertx.docgen;
   requires static io.vertx.codegen.api;
   requires static io.vertx.codegen.json;
   requires com.google.protobuf;
