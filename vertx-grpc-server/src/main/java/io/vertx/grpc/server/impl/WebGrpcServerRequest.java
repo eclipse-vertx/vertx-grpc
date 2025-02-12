18bf08b73394f77aa0c8ab2205af020418ff7cb8
Julien Viet <julien@julienviet.com>
2025-02-12 18:10:23 +0100

    Add grpc-web deframer

--- vertx-grpc-server/src/main/java/io/vertx/grpc/server/impl/WebGrpcServerRequest.java
+++ vertx-grpc-server/src/main/java/io/vertx/grpc/server/impl/WebGrpcServerRequest.java
@@ -10,8 +10,6 @@
  */
 package io.vertx.grpc.server.impl;
 
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
 import io.netty.handler.codec.base64.Base64;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.HttpServerRequest;
@@ -19,8 +17,10 @@ import io.vertx.core.http.HttpVersion;
 import io.vertx.core.internal.ContextInternal;
 import io.vertx.core.internal.buffer.BufferInternal;
 import io.vertx.grpc.common.GrpcMediaType;
+import io.vertx.grpc.common.GrpcMessage;
 import io.vertx.grpc.common.GrpcMessageDecoder;
 import io.vertx.grpc.common.WireFormat;
+import io.vertx.grpc.common.impl.GrpcMessageDeframer;
 import io.vertx.grpc.common.impl.GrpcMethodCall;
 import io.vertx.grpc.common.impl.Http2GrpcMessageDeframer;
 import io.vertx.grpc.server.GrpcProtocol;
@@ -29,59 +29,55 @@ import static io.vertx.core.http.HttpHeaders.CONTENT_TYPE;
 
 public class WebGrpcServerRequest<Req, Resp> extends GrpcServerRequestImpl<Req, Resp> {
 
-  private static final BufferInternal EMPTY_BUFFER = BufferInternal.buffer(Unpooled.EMPTY_BUFFER);
+  static class TextMessageDeframer implements GrpcMessageDeframer {
 
-  private BufferInternal grpcWebTextBuffer;
+    private boolean ended;
+    private boolean processed;
+    private Buffer buffer;
+
+    @Override
+    public void update(Buffer chunk) {
+      if (buffer == null) {
+        buffer = chunk;
+      } else {
+        try {
+          buffer.appendBuffer(chunk);
+        } catch (IndexOutOfBoundsException e) {
+          // Work around because we cannot happend to slices
+          //          java.lang.IndexOutOfBoundsException: writerIndex(270) + minWritableBytes(120) exceeds maxCapacity(270): UnpooledSlicedByteBuf(ridx: 0, widx: 270, cap: 270/270, unwrapped: VertxUnsafeHeapByteBuf(ridx: 0, widx: 0, cap: 270))
+          //          at io.netty.buffer@4.2.0.RC3/io.netty.buffer.AbstractByteBuf.ensureWritable0(AbstractByteBuf.java:294)
+          //          at io.netty.buffer@4.2.0.RC3/io.netty.buffer.AbstractByteBuf.ensureWritable(AbstractByteBuf.java:280)
+          //          at io.netty.buffer@4.2.0.RC3/io.netty.buffer.AbstractByteBuf.writeBytes(AbstractByteBuf.java:1103)
+          //          at io.vertx.core@5.0.0-SNAPSHOT/io.vertx.core.buffer.impl.BufferImpl.appendBuffer(BufferImpl.java:256)
+          //          at io.vertx.core@5.0.0-SNAPSHOT/io.vertx.core.buffer.impl.BufferImpl.appendBuffer(BufferImpl.java:41)
+          buffer = buffer.copy();
+          buffer.appendBuffer(chunk);
+        }
+      }
+    }
+
+    @Override
+    public void end() {
+      ended = true;
+    }
+
+    @Override
+    public Object next() {
+      if (!ended || processed) {
+        return null;
+      }
+      processed = true;
+      if (buffer == null) {
+        return null;
+      }
+      BufferInternal decoded = BufferInternal.buffer(Base64.decode(((BufferInternal)buffer).getByteBuf()));
+      GrpcMessage ret = GrpcMessage.message("identity", decoded.slice(5, decoded.length()));
+      buffer = null;
+      return ret;
+    }
+  }
 
   public WebGrpcServerRequest(ContextInternal context, boolean scheduleDeadline, GrpcProtocol protocol, WireFormat format, long maxMessageSize, HttpServerRequest httpRequest, GrpcMessageDecoder<Req> messageDecoder, GrpcMethodCall methodCall) {
-    super(context, scheduleDeadline, protocol, format, httpRequest, new Http2GrpcMessageDeframer(maxMessageSize, httpRequest.headers().get("grpc-encoding"), format), messageDecoder, methodCall);
-
-    if (httpRequest.version() != HttpVersion.HTTP_2 && GrpcMediaType.isGrpcWebText(httpRequest.getHeader(CONTENT_TYPE))) {
-      grpcWebTextBuffer = EMPTY_BUFFER;
-    } else {
-      grpcWebTextBuffer = null;
-    }
-  }
-
-  protected final boolean notGrpcWebText() {
-    return grpcWebTextBuffer == null;
- }
-
-  @Override
-  public void handle(Buffer chunk) {
-    if (notGrpcWebText()) {
-      super.handle(chunk);
-    } else {
-      if (grpcWebTextBuffer == EMPTY_BUFFER) {
-        ByteBuf bbuf = ((BufferInternal) chunk).getByteBuf();
-        if ((chunk.length() & 0b11) == 0) {
-          // Content length is divisible by four, so we decode it immediately
-          super.handle(BufferInternal.buffer(Base64.decode(bbuf)));
-        } else {
-          grpcWebTextBuffer = BufferInternal.buffer(bbuf.copy());
-        }
-        return;
-      }
-      bufferAndDecode(chunk);
-    }
-  }
-
-  private void bufferAndDecode(Buffer chunk) {
-    grpcWebTextBuffer.appendBuffer(chunk);
-    int len = grpcWebTextBuffer.length();
-    // Decode base64 content as soon as we have more bytes than a multiple of four.
-    // We could instead wait for the buffer length to be a multiple of four,
-    // But then in the worst case we may have to buffer the whole request.
-    int maxDecodable = len & ~0b11;
-    if (maxDecodable == len) {
-        BufferInternal decoded = BufferInternal.buffer(Base64.decode(grpcWebTextBuffer.getByteBuf()));
-        grpcWebTextBuffer = EMPTY_BUFFER;
-        super.handle(decoded);
-    } else if (maxDecodable > 0) {
-        ByteBuf bbuf = grpcWebTextBuffer.getByteBuf();
-        BufferInternal decoded = BufferInternal.buffer(Base64.decode(bbuf, 0, maxDecodable));
-        grpcWebTextBuffer = BufferInternal.buffer(bbuf.copy(maxDecodable, len - maxDecodable));
-        super.handle(decoded);
-    }
+    super(context, scheduleDeadline, protocol, format, httpRequest, httpRequest.version() != HttpVersion.HTTP_2 && GrpcMediaType.isGrpcWebText(httpRequest.getHeader(CONTENT_TYPE)) ? new TextMessageDeframer() : new Http2GrpcMessageDeframer(maxMessageSize, httpRequest.headers().get("grpc-encoding"), format), messageDecoder, methodCall);
   }
 }
