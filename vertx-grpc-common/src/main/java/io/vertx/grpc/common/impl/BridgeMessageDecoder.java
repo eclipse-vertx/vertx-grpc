b72f6fb8be6de4df05814102cb320be2b545df65
Julien Viet <julien@julienviet.com>
2025-03-05 15:59:31 +0100

    Avoid un-necessary byte copies when decoding a Vert.x Buffer to a protobuf object and perform more optimal allocation.
    Motivation:
    
    The current implementation of the bridge uses a plain ByteArrayInputStream and then let protobuf parse the message. It turns out there is a KnownLength interface used by the parser to know the actual buffer size to be decoded and optimize the decoding process.
    
    In addition we can use Netty ByteBufInputStream to avoid using ByteArrayInputStream and un-necessary copies.
    
    Changes:
    
    Swap usage of ByteArrayInputStream with a class that extends ByteBufInputStream and implement KnownLength.

--- vertx-grpc-common/src/main/java/io/vertx/grpc/common/impl/BridgeMessageDecoder.java
+++ vertx-grpc-common/src/main/java/io/vertx/grpc/common/impl/BridgeMessageDecoder.java
@@ -11,7 +11,11 @@
 package io.vertx.grpc.common.impl;
 
 import io.grpc.Decompressor;
+import io.grpc.KnownLength;
 import io.grpc.MethodDescriptor;
+import io.netty.buffer.ByteBufInputStream;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.buffer.impl.BufferImpl;
 import io.vertx.grpc.common.GrpcMessage;
 import io.vertx.grpc.common.GrpcMessageDecoder;
 
@@ -29,15 +33,31 @@ public class BridgeMessageDecoder<T> implements GrpcMessageDecoder<T> {
     this.decompressor = decompressor;
   }
 
+  private static class KnownLengthStream extends ByteBufInputStream implements KnownLength {
+    public KnownLengthStream(Buffer buffer) {
+      super(((BufferImpl)buffer).getByteBuf(), buffer.length());
+    }
+
+    @Override
+    public void close() {
+      try {
+        super.close();
+      } catch (IOException ignore) {
+      }
+    }
+  }
+
   @Override
   public T decode(GrpcMessage msg) {
-    if (msg.encoding().equals("identity")) {
-      return marshaller.parse(new ByteArrayInputStream(msg.payload().getBytes()));
-    } else {
-      try (InputStream in = decompressor.decompress(new ByteArrayInputStream(msg.payload().getBytes()))) {
-        return marshaller.parse(in);
-      } catch (IOException e) {
-        throw new RuntimeException(e);
+    try (KnownLengthStream kls = new KnownLengthStream(msg.payload())) {
+      if (msg.encoding().equals("identity")) {
+        return marshaller.parse(kls);
+      } else {
+        try (InputStream in = decompressor.decompress(kls)) {
+          return marshaller.parse(in);
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
       }
     }
   }
