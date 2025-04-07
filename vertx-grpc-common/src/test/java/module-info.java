e85b151cd45d1ca360671590e29a15c323f3e228
Julien Viet <julien@julienviet.com>
2025-04-07 22:55:20 +0200

    Add missing requires in io.vertx.tests.common

--- vertx-grpc-common/src/test/java/module-info.java
+++ vertx-grpc-common/src/test/java/module-info.java
@@ -3,6 +3,11 @@ open module io.vertx.tests.common {
   requires io.vertx.grpc.common;
   requires io.vertx.testing.unit;
   requires junit;
+  requires com.google.common;
+  requires com.google.protobuf;
+  requires io.grpc;
+  requires io.grpc.protobuf;
+  requires io.grpc.stub;
   exports io.vertx.tests.common;
   exports io.vertx.tests.common.grpc;
 }
