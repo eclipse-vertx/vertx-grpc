5fbc367a4d6815c2226931e09d792825ff8de3f4
Daniel Fiala <danfiala23@gmail.com>
2025-04-22 19:09:59 +0200

    Refactor health check interval to use Duration.
    Updated `HealthServiceOptions` to replace integer-based intervals with `Duration` for improved clarity and type safety. Adjusted related methods and usages to accommodate this change.

--- vertx-grpc-health/src/main/java/io/vertx/grpc/health/HealthServiceOptions.java
+++ vertx-grpc-health/src/main/java/io/vertx/grpc/health/HealthServiceOptions.java
@@ -12,23 +12,22 @@ package io.vertx.grpc.health;
 
 import io.vertx.codegen.annotations.DataObject;
 import io.vertx.codegen.annotations.Unstable;
-import io.vertx.codegen.json.annotations.JsonGen;
-import io.vertx.core.json.JsonObject;
+
+import java.time.Duration;
 
 /**
  * Configuration for a {@link HealthService}.
  */
-@DataObject
-@JsonGen(publicConverter = false)
 @Unstable
+@DataObject
 public class HealthServiceOptions {
 
   /**
-   * The health check interval in milliseconds, by default = {@code 2500}
+   * The health check interval in milliseconds, by default = {@code 2500} milliseconds.
    */
-  public static final int HEALTH_CHECK_INTERVAL = 2500;
+  public static final Duration HEALTH_CHECK_INTERVAL = Duration.ofMillis(2500);
 
-  private int healthCheckInterval;
+  private Duration healthCheckInterval;
 
   /**
    * Default options.
@@ -45,48 +44,23 @@ public class HealthServiceOptions {
   }
 
   /**
-   * Creates options from JSON.
+   * @return the health check interval in {@link Duration}
    */
-  public HealthServiceOptions(JsonObject json) {
-    this();
-    if (json != null) {
-      if (json.containsKey("healthCheckInterval")) {
-        healthCheckInterval = json.getInteger("healthCheckInterval");
-      }
-    }
-  }
-
-  /**
-   * @return the health check interval in milliseconds
-   */
-  public int getHealthCheckInterval() {
+  public Duration getHealthCheckInterval() {
     return healthCheckInterval;
   }
 
   /**
    * Set the health check interval in milliseconds
+   *
    * @param healthCheckInterval the interval in milliseconds
    * @return a reference to this, so the API can be used fluently
    */
-  public HealthServiceOptions setHealthCheckInterval(int healthCheckInterval) {
-    if (healthCheckInterval <= 0) {
-      throw new IllegalArgumentException("Health check interval must be > 0");
+  public HealthServiceOptions setHealthCheckInterval(Duration healthCheckInterval) {
+    if (healthCheckInterval == null || healthCheckInterval.toMillis() <= 0) {
+      throw new IllegalArgumentException("Health check interval must be > 0 milliseconds. Provided: " + healthCheckInterval + " milliseconds.");
     }
     this.healthCheckInterval = healthCheckInterval;
     return this;
-  }
-
-  /**
-   * @return a JSON representation of options
-   */
-  public JsonObject toJson() {
-    JsonObject json = new JsonObject();
-    json.put("healthCheckInterval", healthCheckInterval);
-    return json;
-  }
-
-  @Override
-  public String toString() {
-    return toJson().encode();
   }
 }
