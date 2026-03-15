89d2d9beb6b983ea691a8d500b1618450e16738f
Julien Viet <julien@julienviet.com>
2026-03-15 23:46:18 +0100

    GrpcClientInvoker should return GrpcStream

--- vertx-grpc-client/src/main/java/io/vertx/grpc/client/impl/GrpcClientInvoker.java
+++ vertx-grpc-client/src/main/java/io/vertx/grpc/client/impl/GrpcClientInvoker.java
@@ -1,9 +1,10 @@
 package io.vertx.grpc.client.impl;
 
 import io.vertx.grpc.common.ServiceName;
+import io.vertx.grpc.common.impl.GrpcStream;
 
 public interface GrpcClientInvoker {
 
-  Http2GrpcInboundStream invoke(ServiceName serviceName, String methodName);
+  GrpcStream invoke(ServiceName serviceName, String methodName);
 
 }
