63f42b87d84e6a3324769611485b9831961c4bc2
Daniel Fiala <danfiala23@gmail.com>
2026-04-11 20:53:48 +0200

    Add gRPC EventBus transport documentation.

---
+++ vertx-grpc-docs/src/main/java/examples/GrpcEventBusExamples.java
@@ -0,0 +1,56 @@
+package examples;
+
+import examples.grpc.*;
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.core.net.SocketAddress;
+import io.vertx.docgen.Source;
+import io.vertx.grpc.common.WireFormat;
+import io.vertx.grpc.eventbus.EventBusGrpcClient;
+import io.vertx.grpc.eventbus.EventBusGrpcServer;
+import io.vertx.grpc.server.Service;
+
+@Source
+public class GrpcEventBusExamples {
+
+  public void createServer(Vertx vertx) {
+    EventBusGrpcServer server = EventBusGrpcServer.create(vertx);
+  }
+
+  public void createClient(Vertx vertx) {
+    EventBusGrpcClient client = EventBusGrpcClient.create(vertx);
+  }
+
+  public void serverWithService(Vertx vertx) {
+    EventBusGrpcServer server = EventBusGrpcServer.create(vertx);
+
+    Service service = GreeterGrpcService.of(new GreeterService() {
+      @Override
+      public Future<HelloReply> sayHello(HelloRequest request) {
+        return Future.succeededFuture(HelloReply.newBuilder()
+          .setMessage("Hello " + request.getName())
+          .build());
+      }
+    });
+
+    server.addService(service);
+  }
+
+  public void clientWithService(Vertx vertx) {
+    EventBusGrpcClient client = EventBusGrpcClient.create(vertx);
+
+    GreeterClient greeter = GreeterGrpcClient.create(client, SocketAddress.inetSocketAddress(0, "localhost"));
+
+    greeter.sayHello(HelloRequest.newBuilder().setName("World").build())
+      .onSuccess(reply -> System.out.println("Received: " + reply.getMessage()));
+  }
+
+  public void jsonWireFormat(Vertx vertx) {
+    EventBusGrpcClient client = EventBusGrpcClient.create(vertx);
+
+    GreeterClient greeter = GreeterGrpcClient.create(client, SocketAddress.inetSocketAddress(0, "localhost"), WireFormat.JSON);
+
+    greeter.sayHello(HelloRequest.newBuilder().setName("World").build())
+      .onSuccess(reply -> System.out.println("Received: " + reply.getMessage()));
+  }
+}
