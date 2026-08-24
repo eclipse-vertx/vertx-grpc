f5c3752316338c72a254cfed9b0ae0ec73e74576
Julien Viet <julien@julienviet.com>
2026-08-24 18:41:43 +0200

    Fix incorrect cancellation frame write failure handling.
    Motivation:
    
    The failure write of a cancellation frame triggers the endpoint to be taken down.
    
    The implementation rely the stream remote endpoint field and this can be set to null when the stream is closed, resulting in not disposing the endpoint.
    
    Changes:
    
    Let the write failure uses the capture endpoint reference to attempt to dispose the endpoint.

---
+++ vertx-grpc-eventbus/src/test/java/io/vertx/grpc/eventbus/tests/EventBusGrpcEndpointTest.java
@@ -0,0 +1,77 @@
+package io.vertx.grpc.eventbus.tests;
+
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.grpc.client.GrpcClientRequest;
+import io.vertx.grpc.common.tests.Reply;
+import io.vertx.grpc.common.tests.Request;
+import io.vertx.grpc.eventbus.EventBusGrpcClient;
+import io.vertx.grpc.eventbus.EventBusGrpcClientOptions;
+import io.vertx.grpc.eventbus.EventBusGrpcServer;
+import io.vertx.grpc.eventbus.EventBusGrpcServerOptions;
+import org.junit.Test;
+
+import java.time.Duration;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class EventBusGrpcEndpointTest extends EventBusGrpcTestBase {
+
+  EventBusGrpcServer server;
+  EventBusGrpcClient client;
+
+  @Override
+  public void setUp(TestContext should) {
+    super.setUp(should);
+    client = EventBusGrpcClient.client(vertx, new EventBusGrpcClientOptions()
+      .setPingInterval(Duration.ofMillis(1))
+    ).await();
+    server = EventBusGrpcServer.server(vertx, new EventBusGrpcServerOptions()).await();
+  }
+
+  @Test
+  public void testCancellationWriteFailureShouldDisposeTheEndpoint(TestContext should) throws Exception {
+
+    Async test = should.async(2);
+
+    AtomicInteger numberOfPingFrames = new AtomicInteger();
+    vertx.eventBus().addOutboundInterceptor(new TransportInterceptor() {
+      @Override
+      protected Result onClientCancel(String serverAddress, String streamId) {
+        return Result.failure(new Exception("could not cancel"));
+      }
+      @Override
+      protected Result onPing(String srcAddress, String dstAddress, long data, boolean ack) {
+        numberOfPingFrames.incrementAndGet();
+        return super.onPing(srcAddress, dstAddress, data, ack);
+      }
+    });
+
+    server.callHandler(PIPE_SERVER, request -> {
+      request.handler(msg -> {
+        request.response().write(Reply.getDefaultInstance());
+      });
+    });
+
+    GrpcClientRequest<Request, Reply> request1 = client.request(PIPE_CLIENT).await();
+    request1.write(Request.getDefaultInstance()).await();
+    request1.exceptionHandler(err -> {
+      request1.exceptionHandler(null);
+      test.countDown();
+    });
+    request1.response().await();
+
+    GrpcClientRequest<Request, Reply> request2 = client.request(PIPE_CLIENT).await();
+    request2.exceptionHandler(err -> {
+      request2.exceptionHandler(null);
+      test.countDown();
+    });
+    request2.write(Request.getDefaultInstance()).await();
+    request1.response().await();
+
+    request2.cancel();
+    numberOfPingFrames.set(0);
+    test.awaitSuccess(20_000);
+    Thread.sleep(10);
+    should.assertEquals(0, numberOfPingFrames.get());
+  }
+}
