4eff3584edd6ad8b74c41dfa6ff2b68643523846
Julien Viet <julien@julienviet.com>
2026-09-02 15:56:23 +0200

    Doc and spec complement / fixes

--- vertx-grpc-docs/src/main/java/examples/GrpcEventBusExamples.java
+++ vertx-grpc-docs/src/main/java/examples/GrpcEventBusExamples.java
@@ -4,11 +4,19 @@ import examples.grpc.*;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import io.vertx.core.streams.ReadStream;
+import io.vertx.core.streams.WriteStream;
 import io.vertx.docgen.Source;
+import io.vertx.grpc.common.GrpcStatus;
 import io.vertx.grpc.common.WireFormat;
 import io.vertx.grpc.eventbus.EventBusGrpcClient;
+import io.vertx.grpc.eventbus.EventBusGrpcClientOptions;
 import io.vertx.grpc.eventbus.EventBusGrpcServer;
+import io.vertx.grpc.eventbus.EventBusGrpcServerOptions;
 import io.vertx.grpc.server.Service;
+import io.vertx.grpc.server.StatusException;
+
+import java.time.Duration;
+import java.util.Set;
 
 @Source
 public class GrpcEventBusExamples {
@@ -21,7 +29,7 @@ public class GrpcEventBusExamples {
     Future<EventBusGrpcClient> client = EventBusGrpcClient.client(vertx);
   }
 
-  public void serverWithService(Vertx vertx) {
+  public void serverWithService(EventBusGrpcServer server) {
     Service service = GreeterGrpcService.of(new GreeterService() {
       @Override
       public Future<HelloReply> sayHello(HelloRequest request) {
@@ -31,48 +39,67 @@ public class GrpcEventBusExamples {
       }
     });
 
-    EventBusGrpcServer.server(vertx).onSuccess(server -> server.addService(service));
+    server.addService(service);
   }
 
-  public void clientWithService(Vertx vertx) {
-    EventBusGrpcClient.client(vertx).onSuccess(client -> {
-      GreeterClient greeter = GreeterGrpcClient.create(client);
+  public void clientWithService(EventBusGrpcClient client) {
+    GreeterClient greeter = GreeterGrpcClient.create(client);
 
-      greeter.sayHello(HelloRequest.newBuilder().setName("World").build())
-        .onSuccess(reply -> System.out.println("Received: " + reply.getMessage()));
-    });
+    greeter.sayHello(HelloRequest.newBuilder().setName("World").build())
+      .onSuccess(reply -> System.out.println("Received: " + reply.getMessage()));
   }
 
-  public void jsonWireFormat(Vertx vertx) {
-    EventBusGrpcClient.client(vertx).onSuccess(client -> {
-      GreeterClient greeter = GreeterGrpcClient.create(client, WireFormat.JSON);
+  public void jsonWireFormat(EventBusGrpcClient client) {
+    GreeterClient greeter = GreeterGrpcClient.create(client, WireFormat.JSON);
 
-      greeter.sayHello(HelloRequest.newBuilder().setName("World").build())
-        .onSuccess(reply -> System.out.println("Received: " + reply.getMessage()));
-    });
+    greeter.sayHello(HelloRequest
+        .newBuilder()
+        .setName("World")
+        .build())
+      .onSuccess(reply -> System.out.println("Received: " + reply.getMessage()));
   }
 
-  public void streamingServer(Vertx vertx) {
+  public void streamingServer(EventBusGrpcServer server) {
     Service service = StreamingGrpcService.of(new StreamingService() {
       @Override
-      public Future<ReadStream<Item>> pipe(ReadStream<Item> request) {
-        return Future.succeededFuture(request);
+      protected void pipe(ReadStream<Item> request, WriteStream<Item> response) {
+        request.handler(item -> response.write(item));
+        request.endHandler(v -> response.end());
       }
     });
 
-    EventBusGrpcServer.server(vertx).onSuccess(server -> server.addService(service));
+    server.addService(service);
   }
 
-  public void streamingClient(Vertx vertx) {
-    EventBusGrpcClient.client(vertx).onSuccess(client -> {
-      StreamingClient streaming = StreamingGrpcClient.create(client);
+  public void streamingClient(EventBusGrpcClient client) {
+    StreamingClient streaming = StreamingGrpcClient.create(client);
 
-      streaming.pipe((stream, err) -> {
+    streaming.pipe((stream, err) -> {
+      if (err == null) {
         stream.write(Item.newBuilder().setValue("a").build());
         stream.write(Item.newBuilder().setValue("b").build());
         stream.end();
-      }).onSuccess(response -> response
-        .handler(item -> System.out.println("Received: " + item.getValue())));
-    });
+      }
+    }).onSuccess(response -> response
+      .handler(item -> System.out.println("Received: " + item.getValue())));
+  }
+
+  public void clientOptions() {
+    EventBusGrpcClientOptions options = new EventBusGrpcClientOptions()
+      .setPingInterval(Duration.ofSeconds(15))
+      .setPingTimeout(Duration.ofSeconds(45))
+      .setInitialWindowSize(128);
+  }
+
+  public void serverOptions(Vertx vertx) {
+    EventBusGrpcServerOptions options = new EventBusGrpcServerOptions()
+      .setEnabledFormats(Set.of(WireFormat.PROTOBUF))
+      .setMaxPingTimeout(Duration.ofMinutes(5));
+  }
+
+  public void closing(EventBusGrpcServer server) {
+    server
+      .close()
+      .onSuccess(v -> System.out.println("Server closed"));
   }
 }
